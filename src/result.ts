02ba2a680a3e398ec03c1d95e947b769b444b6cb
n <nile0729@gmail.com>
2018-08-01 13:10:34 -0400

    getError

--- src/result.ts
+++ src/result.ts
@@ -4,22 +4,40 @@ import { UncheckedResultError } from './errors';
 
 class Resulting<T> {
 
-    private value: Error | T;
+    private value: Error | T | string;
+    private is_fail = false;
 
     private checked = false;
 
-    constructor (value: Error | T) {
+    constructor (value: Error | T | string, is_fail: boolean = false) {
         this.value = value;
+        this.is_fail = is_fail;
     }
 
     isError (): boolean {
         this.checked = true;
+        
+        if (this.is_fail) {
+            return true;
+        }
 
         return this.value instanceof Error;
     }
 
     isOk (): boolean {
         return !this.isError();
+    }
+
+    getError() {
+        if (this.value instanceof Error) {
+            return this.value;
+        }
+
+        if (typeof this.value === 'string') {
+            return new Error(this.value);
+        }
+
+        return this.value as any as Error;
     }
 
     unwrap (): T {
@@ -32,8 +50,8 @@ class Resulting<T> {
 
 };
 
-function Result<T>(val: Error | T) {
-    return new Resulting(val);
+function Result<T>(val: Error | T | string, is_fail: boolean = false) {
+    return new Resulting(val, is_fail);
 }
 
 export {
